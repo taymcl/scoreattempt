01651454bffb84b40921ce244af1bee93cf6d432
mrjasonwalton00 <jpwalton00@gmail.com>
2023-02-03 15:28:45 -0500

    added css and bootstrap

--- base/urls.py
+++ base/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.homePage, name='homePage'),
+    path('/loginPage', views.loginPage, name='loginPage'),
 ]
--- base/views.py
+++ base/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def homePage(request):
     return render(request, 'base/homePage.html')
+
+
+def loginPage(request):
+    return render(request, 'base/loginPage.html')
